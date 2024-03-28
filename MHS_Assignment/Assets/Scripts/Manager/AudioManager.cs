0e9dc5737d78ec65e56dc574e057e7a65345560b
SSTG <67901394+SSTG@users.noreply.github.com>
2024-03-28 18:18:57 +0530

    Division of Scripts + Audio Settings

---
+++ MHS_Assignment/Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : Singleton<AudioManager>
+{
+    // Start is called before the first frame update
+    [SerializeField]AudioClip backgroundMusic;
+    [SerializeField]AudioSource sfxSource;
+    [SerializeField]AudioClip sampleSFXClip;
+    AudioSource audioSource;
+    void Start()
+    {
+
+        audioSource=GetComponent<AudioSource>();
+        audioSource.clip=backgroundMusic;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    public void PlaySFX(AudioClip sfxClip)
+    {
+        sfxSource.clip=sfxClip;
+        sfxSource.Play();
+    }
+    public void ChangeSFXVol(Slider slider)
+    {
+        sfxSource.volume=slider.value;
+        PlaySFX(sampleSFXClip);
+    }
+    public void ChangeMusicVol(Slider slider)
+    {
+        audioSource.volume=slider.value;
+    }
+}
